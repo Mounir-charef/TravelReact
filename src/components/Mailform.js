862fb7c9f7a24a9ddf025bd5458bc0681195d353
Mounir <polomnr1@gmail.com>
2022-11-08 23:36:32 +0100

    adding logic to the contact form

--- src/components/Mailform.js
+++ src/components/Mailform.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import {Button} from "./Button";
+import React, {useState} from 'react';
 import './Form.css';
+import emailjs from 'emailjs-com'
 
-const loggin = ( ) => {
-    console.log('lggin');
-}
 
 function Mailform() {
+
+    const [name, setName] = useState(''),
+        [email, setEmail] = useState(''),
+        [content, setContent] = useState(''),
+        [click, setClick] = useState(false);
+
+
+    const sendEmail = e => {
+        e.preventDefault();
+        emailjs.sendForm('service_czd3flg', 'template_auqe7vo', e.target, 'SGdIuvI3-356t5H34')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        // 👇️ clear all input values in the form
+        setName('');
+        setEmail('');
+        setContent('');
+        setClick(true);
+    }
+
+
     return (
         <div className='form-container'>
             <div className='form-wrap'>
                 <h1 className='form-title'> Contact us </h1>
+                {click && <h3 className='form-confirmation'> Email send </h3>}
                 <div className="form-box">
-                    <form className='form-content'>
+                    <form id='form1' className='form-content' onSubmit={sendEmail}>
 
-                        <input className='form-item' type="text" name='name' placeholder='Name'/>
-                        <input className='form-item' type="email" name='email' placeholder='Email'/>
-                        <textarea className='form-item' name="content" id="text" cols="30" rows="5" placeholder='Message'></textarea>
+                        <input className='form-item' type="text" name='name' placeholder='Name' value={name} onChange={e => setName(e.target.value)} required/>
+                        <input className='form-item' type="email" name='email' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} required/>
+                        <textarea className='form-item' name="content" id="text" cols="30" rows="5" placeholder='Message' onChange={e => setContent(e.target.value)} value={content} required></textarea>
 
-                        <Button buttonStyle='btn--outline' buttonSize='btn--medium' type='submit' onClick={loggin}>
-                            SEND
-                        </Button>
+                        <input type="submit" className='submit--outline submit--medium'/>
+
                     </form>
                 </div>
             </div>
