585a6aa96e830a1421e408462143ba8bef5603a4
Mounir <polomnr1@gmail.com>
2022-11-18 17:27:33 +0100

    compress images for better loading time

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,24 +8,24 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <Carditem src={require('../images/img-9.jpg')}
+                        <Carditem src={require('../images/img-9.webp')}
                                   text="Explore the hidden jungle in Africa u bois"
                                   label="Adventure" path='/services'
                         />
-                        <Carditem src={require('../images/img-4.jpg')}
+                        <Carditem src={require('../images/img-4.webp')}
                                       text="Play your best football game on an Island"
                                   label="Island" path='/services'
                         />
                     </ul>
 
                     <ul className="cards__items">
-                        <Carditem src={require('../images/img-2.jpg')}
+                        <Carditem src={require('../images/img-2.webp')}
                                   text="Explore Best beaches"
                                   label="Beach" path='/services'/>
-                        <Carditem src={require('../images/img-3.jpg')}
+                        <Carditem src={require('../images/img-3.webp')}
                                   text="Go deeper than anyone in the open sea"
                                   label="Deep Sea" path='/services'/>
-                        <Carditem src={require('../images/img-7.jpg')}
+                        <Carditem src={require('../images/img-7.webp')}
                                   text="Discover your self and live your best life"
                                   label="Discover" path='/services'/>
                     </ul>
