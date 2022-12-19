241ce338ad363b72382bdfee8a779623bcb44a62
Mounir <polomnr1@gmail.com>
2022-12-19 11:12:32 +0100

    fix number of posts and scroll bare behaviour

--- src/App.js
+++ src/App.js
@@ -5,27 +5,23 @@ import {Routes , Route} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import SignUp from "./components/pages/SignUp";
-import Contact from "./components/pages/Contact";
 import PuffLoader from 'react-spinners/PuffLoader';
-
+import {lazy, Suspense} from "react";
+const Contact = lazy(() => import("./components/pages/Contact"))
 
 const override = {
     display: "block",
     margin: "0 auto",
     translate : "0 40vh",
-    borderColor: "red",
     color: "202020"
 };
 
 function App() {
 
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     useEffect(()=> {
-        setLoading(true)
-        setTimeout(()=>{
-            setLoading(false)
-        }, 5000)
+        setTimeout(() => setLoading(false), 4000);
     }, [])
 
   return (
@@ -34,7 +30,6 @@ function App() {
           size={50}
           color="#252525"
           cssOverride={override}
-          loading={loading}
           aria-label="Loading Spinner"
           data-testid="loader"
       /> }
@@ -42,12 +37,14 @@ function App() {
         {!loading && <>
             <Navbar />
             <Routes>
-                <Route path='/' element={
-                    <Home/>
-                }/>
+                <Route path='/' element={<Home/>}/>
                 <Route path='/services' element={<Services/>} />
                 <Route path='/sign-up' element={<SignUp/>} />
-                <Route path='/contact' element={<Contact />} />
+                <Route path='/contact' element={
+                    <Suspense fallback={<p style={{textAlign: "center"}}> Loading ... </p>}>
+                        <Contact />
+                    </Suspense>
+                } />
             </Routes>
         </>}
       </>
