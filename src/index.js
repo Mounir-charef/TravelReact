d03fa078b5ab4b978f7d510c31d404fb88eaa32b
Mounir <polomnr1@gmail.com>
2023-03-02 22:03:02 +0100

    updating router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import {HashRouter} from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+import './App.css';
+// import App from './App';
+// import {HashRouter} from "react-router-dom";
+import {
+    createBrowserRouter, Outlet,
+    RouterProvider
+} from "react-router-dom";
+const Contact = lazy(() => import("./components/pages/Contact"))
+const routes = [
+        { path: '/', element: <Home />},
+        { path: 'services', element: <Services /> },
+        { path: 'sign-up', element: <SignUp /> },
+        {
+            path: 'contact', element: (
+                <Suspense fallback={<p style={{textAlign: "center"}}> Loading ... </p>}>
+                    <Contact />
+                </Suspense>
+            ),
+        },
+]
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+        </>
+    )
+}
+
+const router = createBrowserRouter([{element: <App />, children: routes}])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <RouterProvider router={router} />
     </React.StrictMode>,
 );
 
